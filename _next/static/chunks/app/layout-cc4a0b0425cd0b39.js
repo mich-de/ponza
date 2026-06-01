25b9772c804c8d1cb5043b3cd687dfdc20b1d258
mich-de <mich-de@users.noreply.github.com>
2026-06-01 15:53:37 +0000

    deploy: a8d964330ad9cbc417fc4edb833e8162b40be778

--- _next/static/chunks/app/layout-cc4a0b0425cd0b39.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{963:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,41290,23))},41290:()=>{}},e=>{e.O(0,[741,441,255,358],()=>e(e.s=963)),_N_E=e.O()}]);
